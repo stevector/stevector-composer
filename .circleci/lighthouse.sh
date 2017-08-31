0f3b23ef367b6324e64cf1f796ecfef98de47751
Steve Persch <stevector@users.noreply.github.com>
2017-08-31 16:34:47 -0500

    Update lighthouse.sh

--- .circleci/lighthouse.sh
+++ .circleci/lighthouse.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-
-echo ${CIRCLE_PULL_REQUEST}
+# todo, rather than apt-getting curl here, it might be enough to save the artifact and let a later job do all the reporting.
+apt-get curl
 
 PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io
 
@@ -14,4 +14,5 @@ lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save
 ARTIFACT_URL="https://circleci.com/api/v1.1/project/github/stevector/stevector-composer/${CIRCLE_BUILD_NUM}/artifacts"
 
 comment="### Lighthouse report: \n ${ARTIFACT_URL}"
+
 curl -d '{ "body": "'"$comment"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN
