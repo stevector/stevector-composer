f88df2252d13c5f2a9fc0a0f7c00401f521c0ca3
Steve Persch <steve.persch@pantheon.io>
2017-08-31 16:23:41 -0500

    merging master

--- web/wp-config.php
+++ web/wp-config.php
@@ -192,3 +192,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+
+
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+    // Redirect to https://$primary_domain/ in the Live environment
+    if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+        /** Replace www.example.com with your registered domain name */
+        // @todo update primary domain.
+        // https://github.com/stevector/stevector-composer/issues/10
+        $primary_domain = 'live-stevector-composer.pantheonsite.io';
+    }
+    else {
+        // Redirect to HTTPS on every Pantheon environment.
+        $primary_domain = $_SERVER['HTTP_HOST'];
+    }
+    $base_url = 'https://'. $primary_domain;
+    define('WP_SITEURL', $base_url);
+    define('WP_HOME', $base_url);
+    if ($_SERVER['HTTP_HOST'] != $primary_domain
+        || !isset($_SERVER['HTTP_X_SSL'])
+        || $_SERVER['HTTP_X_SSL'] != 'ON' ) {
+
+        # Name transaction "redirect" in New Relic for improved reporting (optoinal)
+        if (extension_loaded('newrelic')) {
+            newrelic_name_transaction("redirect");
+        }
+
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: '. $base_url . $_SERVER['REQUEST_URI']);
+        exit();
+    }
+}
---
+++ web/wp-content/plugins/timber/lib/timber-page.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @ignore
+ * @deprecated 0.21.9
+ */
+class TimberPage extends TimberPost {
+
+}
