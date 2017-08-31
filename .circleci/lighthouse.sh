6b708a498d68790d9b6e3402b5ab444d9fe14e91
Steve Persch <steve.persch@pantheon.io>
2017-08-31 14:45:34 -0500

    lighthouse script

--- .circleci/lighthouse.sh
+++ .circleci/lighthouse.sh
@@ -5,15 +5,12 @@ set -ex
 
 PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io
 
-# rm -r /tmp/lighthouse-report || echo 'return true even if that dir didn't exist';
 mkdir /tmp/lighthouse-report
 cd /tmp/lighthouse-report
-lighthouse --chrome-flags="--headless --disable-gpu" https://www.stevector.com/ --save-artifacts --save-assets
-
-
-artifact_base_url="https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM/artifacts/0$CIRCLE_ARTIFACTS"
+lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets
 
 ARTIFACT_URL="https://circleci.com/api/v1.1/project/github/stevector/stevector-composer/${CIRCLE_BUILD_NUM}/artifacts"
 
-comment="### Lighthouse report:"
-
+comment="### Lighthouse report: \n ${ARTIFACT_URL}"
+token="$(composer config --global github-oauth.github.com)"
+curl -d '{ "body": "'"$comment"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$token
