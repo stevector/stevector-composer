aea133e8ac8d9446b560391de2491df5b67e06b6
Steve Persch <steve.persch@pantheon.io>
2017-09-24 13:54:27 -0500

    Some code sniff fixes

--- web/wp-content/themes/stevector/functions.php
+++ web/wp-content/themes/stevector/functions.php
@@ -6,7 +6,7 @@
  *
  * @link https://wordpress.org/support/topic/emoji-and-smiley-js-and-css-added-to-head/page/2#post-7087304
  */
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
