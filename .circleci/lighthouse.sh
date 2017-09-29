97f5fb9403146a964d121d5bf6f020079474c0e5
Steve Persch <stevector@users.noreply.github.com>
2017-09-29 13:36:59 -0500

    Update lighthouse.sh

--- .circleci/lighthouse.sh
+++ .circleci/lighthouse.sh
@@ -4,8 +4,15 @@ set -ex
 
 PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io
 
-mkdir /tmp/lighthouse-report
-cd /tmp/lighthouse-report
+ Make artifacts directory
+CIRCLE_ARTIFACTS='artifacts'
+CIRCLE_ARTIFACTS_DIR='/tmp/lighthouse-report'
+mkdir -p $CIRCLE_ARTIFACTS_DIR
+
+# Stash Circle Artifacts URL
+CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
+
+cd CIRCLE_ARTIFACTS_DIR
 lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --perf
 
 ARTIFACT_URL="https://circleci.com/api/v1.1/project/github/stevector/stevector-composer/${CIRCLE_BUILD_NUM}/artifacts"
@@ -14,6 +21,8 @@ ARTIFACT_URL="https://circleci.com/api/v1.1/project/github/stevector/stevector-c
 echo $CIRCLE_ARTIFACTS
 
 echo $ARTIFACT_URL
+
+echo $CIRCLE_ARTIFACTS_URL
 
 #comment="### Lighthouse report: \n ${ARTIFACT_URL}"
 
