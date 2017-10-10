b5ddefaa338da59a08d53446f9f2c990b484745c
Steve Persch <steve.persch@pantheon.io>
2017-10-10 14:15:24 -0500

    uncomment curl

--- backstop/run-backstop.sh
+++ backstop/run-backstop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -e
 
 # Make artifacts directory
 CIRCLE_ARTIFACTS='artifacts'
@@ -34,4 +34,4 @@ report_url=${CIRCLE_ARTIFACTS_URL}/backstop_data/html_report/index.html
 report_link="[![Visual report]($diff_image_url)]($report_url)"
 comment="### Visual regression report:"
 
-# curl -d '{ "body": "'"$comment\\n\\n$report_link"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN
+curl -d '{ "body": "'"$comment\\n\\n$report_link"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN
