2fd645095320917627e6fdd75b73e0d851fc994c
Steve Persch <steve.persch@pantheon.io>
2017-10-10 11:56:40 -0500

    Adding run-backstop.sh

---
+++ backstop/run-backstop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Make artifacts directory
+CIRCLE_ARTIFACTS='artifacts'
+CIRCLE_ARTIFACTS_DIR='/tmp/artifacts'
+mkdir -p $CIRCLE_ARTIFACTS_DIR
+
+# Stash Circle Artifacts URL
+CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
+
+
+# Update the URLs in the backstop file to use the new multidev
+sed -i -e "s/dev-${TERMINUS_SITE}/${TERMINUS_ENV}-${TERMINUS_SITE}/g" ~/$CIRCLE_PROJECT_REPONAME/backstop/backstop.json
+
+backstop reference
+VISUAL_REGRESSION_RESULTS=$(backstop test || echo 'true')
+
+rsync -rlvz backstop_data $CIRCLE_ARTIFACTS_DIR
+
+cd $CIRCLE_ARTIFACTS_DIR
+diff_image=$(find * | grep png | grep diff | head -n 1)
+diff_image_url=${CIRCLE_ARTIFACTS_URL}/$diff_image
+report_url=${CIRCLE_ARTIFACTS_URL}/backstop_data/html_report/index.html
+report_link="[![Visual report]($diff_image_url)]($report_url)"
+comment="### Visual regression report:"
+
+token="$(composer config --global github-oauth.github.com)"
+curl -d '{ "body": "'"$comment\\n\\n$report_link"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$token
