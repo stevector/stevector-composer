bb683e1c33a733af136f28c014f0b32b3aa932bf
Steve Persch <steve.persch@pantheon.io>
2017-10-10 18:40:09 -0500

    Lighthouse reports

--- .circleci/lighthouse/lighthouse.sh
+++ .circleci/lighthouse/lighthouse.sh
@@ -9,11 +9,18 @@ CIRCLE_ARTIFACTS='artifacts'
 CIRCLE_ARTIFACTS_DIR='/tmp/artifacts'
 mkdir -p $CIRCLE_ARTIFACTS_DIR
 
-# Stash Circle Artifacts URL
-CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
+
+
+echo ${BASH_SOURCE[0]}
+LIGHTHOUSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --config-path=${LIGHTHOUSE_DIR}/no_pwa.js
 
 cd $CIRCLE_ARTIFACTS_DIR
-lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --perf
+
+exit 0;
+
+# Stash Circle Artifacts URL
+CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
 HTML_REPORT=$(find * -type f -name "*report.html" | head -n 1)
 REPORT_URL="${CIRCLE_ARTIFACTS_URL}/${HTML_REPORT}"
 COMMENT="### Lighthouse report: \n ${REPORT_URL}"
