12a5e0d0aab6d3c1265af2813d2654bfc7cc36a6
Steve Persch <steve.persch@pantheon.io>
2017-10-12 16:54:50 -0500

    Adding a sleep

--- .circleci/lighthouse/lighthouse.sh
+++ .circleci/lighthouse/lighthouse.sh
@@ -5,7 +5,7 @@ set -ex
 LIGHTHOUSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 node ${LIGHTHOUSE_DIR}/node_modules/lighthouse-ci/frontend/test_started_notification.js
-
+sleep 60
 
 PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io
 
@@ -22,6 +22,11 @@ cd $CIRCLE_ARTIFACTS_DIR
 lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --config-path=${LIGHTHOUSE_DIR}/no_pwa.js --output=json --output=html
 
 JSON_REPORT=$(find * -type f -name "*report.json" | head -n 1)
+
+
+
+
+
 node ${LIGHTHOUSE_DIR}/pass_fail_pr.js $CIRCLE_ARTIFACTS_DIR/$JSON_REPORT
 
 
@@ -29,7 +34,6 @@ HTML_REPORT=$(find * -type f -name "*report.html" | head -n 1)
 REPORT_URL="${CIRCLE_ARTIFACTS_URL}/${HTML_REPORT}"
 COMMENT="### Lighthouse report: \n ${REPORT_URL}"
 
-sleep 60
 
 {
 curl -d '{ "body": "'"$COMMENT"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN
