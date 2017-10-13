958b43a21ea83d880f688c92531983657ac7329f
Steve Persch <steve.persch@pantheon.io>
2017-10-13 16:29:50 -0500

    using REPORT_IRL

--- .circleci/lighthouse/lighthouse.sh
+++ .circleci/lighthouse/lighthouse.sh
@@ -19,12 +19,16 @@ CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ART
 cd $CIRCLE_ARTIFACTS_DIR
 lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --config-path=${LIGHTHOUSE_SCRIPTS_DIR}/no_pwa.js --output=json --output=html
 
-JSON_REPORT=$(find * -type f -name "*report.json" | head -n 1)
-node ${LIGHTHOUSE_SCRIPTS_DIR}/pass_fail_pr.js $CIRCLE_ARTIFACTS_DIR/$JSON_REPORT
 
 HTML_REPORT=$(find * -type f -name "*report.html" | head -n 1)
 REPORT_URL="${CIRCLE_ARTIFACTS_URL}/${HTML_REPORT}"
 COMMENT="### Lighthouse report: \n ${REPORT_URL}"
+
+
+
+JSON_REPORT=$(find * -type f -name "*report.json" | head -n 1)
+node ${LIGHTHOUSE_SCRIPTS_DIR}/pass_fail_pr.js $CIRCLE_ARTIFACTS_DIR/$JSON_REPORT REPORT_URL
+
 
 {
 curl -d '{ "body": "'"$COMMENT"'" }' -X POST https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN
