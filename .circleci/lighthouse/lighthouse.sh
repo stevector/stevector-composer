2acf02ec8f485a1766ba51196e4e3800aa1b9d98
Steve Persch <stevector@users.noreply.github.com>
2017-10-13 12:18:21 -0500

    Update lighthouse.sh

--- .circleci/lighthouse/lighthouse.sh
+++ .circleci/lighthouse/lighthouse.sh
@@ -5,17 +5,6 @@ set -ex
 LIGHTHOUSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIGHTHOUSE_SCRIPTS_DIR=${LIGHTHOUSE_DIR}/node_modules/lighthouse-ci/frontend
 
-node ${LIGHTHOUSE_SCRIPTS_DIR}/test_started_notification.js
-PANTHEON_SITE_URL=https://${TERMINUS_ENV}-${TERMINUS_SITE}.pantheonsite.io
-
-# Make artifacts directory
-CIRCLE_ARTIFACTS='artifacts'
-CIRCLE_ARTIFACTS_DIR='/tmp/artifacts'
-mkdir -p $CIRCLE_ARTIFACTS_DIR
-# Stash Circle Artifacts URL
-CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
-
-
 cd $CIRCLE_ARTIFACTS_DIR
 lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --config-path=${LIGHTHOUSE_SCRIPTS_DIR}/no_pwa.js --output=json --output=html
 
