44bb5a6d542c60be6ef3130ace4dfc04a93d87fb
Steve Persch <steve.persch@pantheon.io>
2017-10-18 16:28:20 -0500

    Git clone install of lighthouse-ci

--- .circleci/lighthouse/lighthouse.sh
+++ .circleci/lighthouse/lighthouse.sh
@@ -3,7 +3,7 @@
 set -ex
 
 LIGHTHOUSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LIGHTHOUSE_SCRIPTS_DIR=${LIGHTHOUSE_DIR}/node_modules/lighthouse-ci/frontend
+LIGHTHOUSE_SCRIPTS_DIR=${LIGHTHOUSE_DIR}/frontend
 
 cd $CIRCLE_ARTIFACTS_DIR
 lighthouse --chrome-flags="--headless --disable-gpu" ${PANTHEON_SITE_URL} --save-artifacts --save-assets --config-path=${LIGHTHOUSE_SCRIPTS_DIR}/no_pwa.js --output=json --output=html
