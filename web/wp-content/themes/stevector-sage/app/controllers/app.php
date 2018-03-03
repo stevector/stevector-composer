11fc4ba0b825f6d5e6355596142f1bc7bed9b58b
Steve Persch <steve.persch@pantheon.io>
2018-03-02 23:40:06 -0600

    Comment out unneeded JS and change homepage h1

--- web/wp-content/themes/stevector-sage/app/controllers/app.php
+++ web/wp-content/themes/stevector-sage/app/controllers/app.php
@@ -17,7 +17,7 @@ class App extends Controller
             if ($home = get_option('page_for_posts', true)) {
                 return get_the_title($home);
             }
-            return __('Latest Posts', 'sage');
+            return __('Blog posts', 'sage');
         }
         if (is_archive()) {
             return get_the_archive_title();
