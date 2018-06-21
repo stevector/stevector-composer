49923bf7b04257f453f68b639f6f7cf007a5ac6a
Steve Persch <steve.persch@pantheon.io>
2018-06-20 21:29:39 -0500

    stevector sage

---
+++ web/wp-content/themes/stevector-sage/resources/functions.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Do not edit anything in this file unless you know what you're doing
+ */
+
+use Roots\Sage\Config;
+use Roots\Sage\Container;
+
+/**
+ * Helper function for prettying up errors
+ * @param string $message
+ * @param string $subtitle
+ * @param string $title
+ */
+$sage_error = function ($message, $subtitle = '', $title = '') {
+    $title = $title ?: __('Sage &rsaquo; Error', 'sage');
+    $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
+    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
+    wp_die($message, $title);
+};
+
+/**
+ * Ensure compatible version of PHP is used
+ */
+if (version_compare('7', phpversion(), '>=')) {
+    $sage_error(__('You must be using PHP 7 or greater.', 'sage'), __('Invalid PHP version', 'sage'));
+}
+
+/**
+ * Ensure compatible version of WordPress is used
+ */
+if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
+    $sage_error(__('You must be using WordPress 4.7.0 or greater.', 'sage'), __('Invalid WordPress version', 'sage'));
+}
+
+/**
+ * Ensure dependencies are loaded
+ */
+if (!class_exists('Roots\\Sage\\Container')) {
+    if (!file_exists($composer = __DIR__.'/../vendor/autoload.php')) {
+        $sage_error(
+            __('You must run <code>composer install</code> from the Sage directory.', 'sage'),
+            __('Autoloader not found.', 'sage')
+        );
+    }
+    require_once $composer;
+}
+
+/**
+ * Sage required files
+ *
+ * The mapped array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ */
+array_map(function ($file) use ($sage_error) {
+    $file = "../app/{$file}.php";
+    if (!locate_template($file, true, true)) {
+        $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
+    }
+}, ['helpers', 'setup', 'filters', 'admin']);
+
+/**
+ * Here's what's happening with these hooks:
+ * 1. WordPress initially detects theme in themes/sage/resources
+ * 2. Upon activation, we tell WordPress that the theme is actually in themes/sage/resources/views
+ * 3. When we call get_template_directory() or get_template_directory_uri(), we point it back to themes/sage/resources
+ *
+ * We do this so that the Template Hierarchy will look in themes/sage/resources/views for core WordPress themes
+ * But functions.php, style.css, and index.php are all still located in themes/sage/resources
+ *
+ * This is not compatible with the WordPress Customizer theme preview prior to theme activation
+ *
+ * get_template_directory()   -> /srv/www/example.com/current/web/app/themes/sage/resources
+ * get_stylesheet_directory() -> /srv/www/example.com/current/web/app/themes/sage/resources
+ * locate_template()
+ * ├── STYLESHEETPATH         -> /srv/www/example.com/current/web/app/themes/sage/resources/views
+ * └── TEMPLATEPATH           -> /srv/www/example.com/current/web/app/themes/sage/resources
+ */
+array_map(
+    'add_filter',
+    ['theme_file_path', 'theme_file_uri', 'parent_theme_file_path', 'parent_theme_file_uri'],
+    array_fill(0, 4, 'dirname')
+);
+Container::getInstance()
+    ->bindIf('config', function () {
+        return new Config([
+            'assets' => require dirname(__DIR__).'/config/assets.php',
+            'theme' => require dirname(__DIR__).'/config/theme.php',
+            'view' => require dirname(__DIR__).'/config/view.php',
+        ]);
+    }, true);
+
+// From https://kinsta.com/knowledgebase/disable-emojis-wordpress/#disable-emojis-code
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ *
+ * @param array $plugins
+ * @return array Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @param array $urls URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed for.
+ * @return array Difference betwen the two arrays.
+ */
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+    if ( 'dns-prefetch' == $relation_type ) {
+        /** This filter is documented in wp-includes/formatting.php */
+        $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+        $urls = array_diff( $urls, array( $emoji_svg_url ) );
+    }
+
+    return $urls;
+}
+
+// https://kinsta.com/knowledgebase/disable-embeds-wordpress/
+
+function my_deregister_scripts(){
+    wp_dequeue_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
