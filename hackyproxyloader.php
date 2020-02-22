cfdc426ff7be1564044f2a646b688b6e120ed03a
Steve Persch <steve.persch@pantheon.io>
2020-02-22 07:06:57 -0600

    load the loader dude

--- hackyproxyloader.php
+++ hackyproxyloader.php
@@ -1,3 +1,6 @@
 <?php
+
 // @TODO Read from ENV or app-config
 $url = 'https://us-central1-serverlessplayground.cloudfunctions.net/';
+
+$hackyproxy = new \Stevector\HackyProxy\PantheonToGCPBucket($url);
